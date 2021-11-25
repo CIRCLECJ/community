21ccf9efec2066ecc7e0c3a5734bd58672188b57
yjh <y1291249384@163.com>
2021-11-25 17:56:31 +0800

    First commit

---
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -0,0 +1,50 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/index", method = RequestMethod.GET)
+    public String getIndexPage(Model model, Page page) {
+        // 方法调用前,SpringMVC会自动实例化Model和Page,并将Page注入Model.
+        // 所以,在thymeleaf中可以直接访问Page对象中的数据.
+        page.setRows(discussPostService.findDiscussPostRows(0));
+        page.setPath("/index");
+
+        List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
+        List<Map<String, Object>> discussPosts = new ArrayList<>();
+        if (list != null) {
+            for (DiscussPost post : list) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("post", post);
+                User user = userService.findUserById(post.getUserId());
+                map.put("user", user);
+                discussPosts.add(map);
+            }
+        }
+        //如果单用discussPostService只能获取到帖子的userId但是我们想要用户名字，所以用了userService通过id获取user对象
+        //然后使用一个map装载这两种数据（discussPost和相应的user对象）
+        model.addAttribute("discussPosts", discussPosts);
+        return "/index";//返回templates下的index.html页面
+    }
+}
---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,21 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+    //可以搜所有帖子也可以搜我的帖子(通过if判断实现)
+    // 后两个参数是分页用的，offset是起始行号，limit是每一页最多显示多少行数据
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    //查询帖子的行数
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.如果不止一个，如selectDiscussPosts也需要用if但有三个参数就可以不写
+    // 如果我需要动态的拼一个条件，并且这个方法有且只有这一个条件，这时参数必须取别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
