070845b9d08b094f69f1fd74528f9a1e5976ed27
yjh <y1291249384@163.com>
2021-12-09 15:53:13 +0800

    优化性能（热帖列表缓存，Caffeine本地缓存）

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -1,27 +1,112 @@
 package com.nowcoder.community.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.util.SensitiveFilter;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
+
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
     @Autowired
     private SensitiveFilter sensitiveFilter;
 
+    @Value("${caffeine.posts.max-size}")
+    private int maxSize;
+
+    @Value("${caffeine.posts.expire-seconds}")
+    private int expireSeconds;
+
+    // Caffeine核心接口: Cache, LoadingCache, AsyncLoadingCache
+    // LoadingCache比较常用，它是同步缓存，把数据取好之后再返回，取好之前就等着；AsyncLoadingCache是异步
+
+    // 帖子列表缓存（所有的缓存都是一样，用key缓存value）
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+
+    // 帖子总数缓存（我们也可以对帖子总数进行缓存，提高性能）
+    private LoadingCache<Integer, Integer> postRowsCache;
+
+    //上面声明了两个缓存变量但是还没有初始化，什么时候初始化呢，
+    // 因为初始化只需要执行一次，因此选择在当前类声明一个初始化方法，这个注解就是说在这个类的构造器之后执行这个方法（只执行一次）
+    @PostConstruct
+    public void init() {
+        // 初始化帖子列表缓存（初始化缓存的方法是固定的，可以在caffeine官网查到）
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    //匿名实现。作用是提供一个查询数据得到初始化数据的办法【即缓存的数据来源是怎么来的】（CacheLoader接口的load方法）
+                    @Nullable
+                    @Override
+                    public List<DiscussPost> load(@NonNull String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("参数错误!");
+                        }
+
+                        String[] params = key.split(":");//切割key字符串
+                        if (params == null || params.length != 2) {
+                            throw new IllegalArgumentException("参数错误!");
+                        }
+
+                        int offset = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+
+                        // 二级缓存: Redis -> mysql
+
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0, offset, limit, 1);
+                    }
+                });
+        // 初始化帖子总数缓存
+        postRowsCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Nullable
+                    @Override
+                    public Integer load(@NonNull Integer key) throws Exception {
+                        logger.debug("load post rows from DB.");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
+
+
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode) {
-        return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);
+        if (userId == 0 && orderMode == 1) {//首页是不传userId的，并且只有在我们要查的是热帖的时候才使用缓存
+            return postListCache.get(offset + ":" + limit);
+            //这里解释了为什么缓存的key用string，每一页与什么有关，与offset和limit有关，因此key就存这两个值的拼接
+        }
+
+        logger.debug("load post list from DB.");
+        return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);//否则还是访问数据库
     }
 
     public int findDiscussPostRows(int userId) {
+        if (userId == 0) {//首页不传userId即为0，其他情况都不使用缓存
+            return postRowsCache.get(userId);//其实这里不需要key，但是缓存必须要有，所以我们还是用userId做key（等于0）
+        }
+        logger.debug("load post rows from DB.");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
